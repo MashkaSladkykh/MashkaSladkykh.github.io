c8f5fe2128d3f26b1e0ce936c7239b98ac92c496
Masha <masha.sladky@gmail.com>
2022-11-30 17:37:02 +0200

    deleted extra import

--- src/pages/JobDetails/component.jsx
+++ src/pages/JobDetails/component.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { useSelector} from 'react-redux';
 import { useParams } from "react-router-dom";
 import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 import ShareIcon from '@mui/icons-material/Share';
