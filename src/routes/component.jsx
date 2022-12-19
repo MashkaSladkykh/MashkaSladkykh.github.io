c84644e09f30405862ded536ea85ac2f01d33467
Masha <masha.sladky@gmail.com>
2022-12-19 13:21:59 +0200

    revert

--- src/routes/component.jsx
+++ src/routes/component.jsx
@@ -1,25 +1,24 @@
-import React from "react";
 import {Route, Routes} from "react-router-dom";
-
 import Jobs from "../pages/Jobs";
 import JobDetails from "../pages/JobDetails";
 import Error from "../pages/Error";
 
+
 const JobApp = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Jobs/>}/>
-      <Route path=":jobId" element={<JobDetails/>}/>
-      <Route
-        path="*"
-        element={
-          <main style={{padding: "1rem"}}>
-            <Error/>
-          </main>
-        }
-      />
-    </Routes>
-  )
-}
+    return (
+      <Routes>
+        <Route path="/" element={<Jobs/>}/>
+        <Route path=":jobId" element={<JobDetails/>}/>
+        <Route
+          path="*"
+          element={
+            <main style={{padding: "1rem"}}>
+              <Error/>
+            </main>
+          }
+        />
+      </Routes>
+    )
+  }
   
   export default JobApp;
